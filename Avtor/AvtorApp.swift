05602b9ef23e2e3b8c3fa3b679540cd54b789698
Viktor <kvv83@mail.ru>
2021-06-04 14:47:02 +0300

    Initial Commit

---
+++ Avtor/AvtorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvtorApp.swift
+//  Avtor
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AvtorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
